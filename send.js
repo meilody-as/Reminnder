5b82d82254de5e5104e9fa1b7469a4e17de11db4
meilody-as <meilody.mt@gmail.com>
2026-01-07 21:04:29 +0700

    Update send.js

--- send.js
+++ send.js
@@ -1,15 +1,19 @@
 const fetch = require('node-fetch');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const CHAT_ID = "ISI_CHAT_ID_ANDA"; // contoh: "123456789"
+const CHAT_ID = "8395291891"; // HANYA ANGKA
 
 (async () => {
-  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+  const res = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      chat_id: 8395291891:AAHOxCGIjGqUmasweoTiYOecyG7ndwcJwnQ,
+      chat_id: 8568224727,
       text: "✅ TEST BERHASIL: GitHub Actions → Telegram OK"
     })
   });
+
+  const data = await res.json();
+  console.log(data);
 })();
+
